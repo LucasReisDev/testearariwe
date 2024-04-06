9e504b2930b9ed4e9b6f7f8af62e3da38b3e837d
LucasReisDev <mcluckluck@hotmail.com>
2024-04-05 23:28:49 -0300

    preparacao do ambiente

---
+++ tasks/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Task(models.Model):
+    titulo = models.CharField(max_length=255)
+    prazo = models.DateField(null=True, blank=True)
+    descricao = models.TextField(blank=True)
+    finalizada = models.BooleanField()
